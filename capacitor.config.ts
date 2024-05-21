f96afd18ee9bff9f7b8a03f4d0395a70a6ea200e
sukrut sathaye <sukrutssathaye@gmail.com>
2024-05-21 17:13:37 +0200

    initial commit of ionic react setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.uniyoga',
+  appName: 'uniyoga',
+  webDir: 'dist'
+};
+
+export default config;
