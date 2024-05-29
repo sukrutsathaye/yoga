ec952bc217aad35f6d64b3c5db8da37c927aed59
sukrut sathaye <sukrutssathaye@gmail.com>
2024-05-29 15:25:59 +0200

    add AuthService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,17 +1,52 @@
 // src/services/AuthService.ts
 
-import { IUser } from '../models/IUser';
+import { 
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut as signOutAuth,
+  UserCredential,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import FirebaseService from './FirebaseService';
+import IAuthService from '../models/IAuthService';
 
-export class AuthService {
-  signIn(user: IUser): void {
-    // Implementation for sign-in logic
+/**
+ * @class AuthService
+ * @implements IAuthService
+ * @description Service for authentication with Firebase
+ * 
+ * @property {Auth} auth - Firebase Auth instance
+ * 
+ * @function signUpWithEmail - Function to sign up with email
+ * @function signInWithEmail - Function to sign in with email
+ * @function signInWithGoogle - Function to sign in with Google
+ * @function signOut - Function to sign out
+ * 
+ */
+export default class AuthService implements IAuthService {
+  protected auth: Auth;
+
+  constructor() {
+    this.auth = FirebaseService.getInstance().getAuth();
+  }
+  
+  
+  public async signUpWithEmail(email: string, password: string): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(this.auth, email, password);
   }
 
-  signOut(user: IUser): void {
-    // Implementation for sign-out logic
+  public async signInWithEmail(email: string, password: string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, email, password);
   }
 
-  updateProfile(user: IUser): void {
-    // Implementation for updating profile logic
+  public async signInWithGoogle(): Promise<UserCredential> {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(this.auth, provider);
+  }
+
+  public async signOut(): Promise<void> {
+    return signOutAuth(this.auth);
   }
 }
